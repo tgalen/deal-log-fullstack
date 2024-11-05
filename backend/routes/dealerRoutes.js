d59042573cc5cad7153023f7f3e063b6030255c9
tgalen <tgalen84@gmail.com>
2024-11-05 15:16:46 -0500

    add post, put, delete routes

--- backend/routes/dealerRoutes.js
+++ backend/routes/dealerRoutes.js
@@ -2,7 +2,19 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.json({ message: "get dealers" });
+  res.status(200).json({ message: "Get dealers" });
+});
+
+router.post("/", (req, res) => {
+  res.status(200).json({ message: "Create dealers" });
+});
+
+router.put("/:id", (req, res) => {
+  res.status(200).json({ message: `Update dealer ${req.params.id}` });
+});
+
+router.delete("/:id", (req, res) => {
+  res.status(200).json({ message: `Delete dealer ${req.params.id}` });
 });
 
 module.exports = router;
