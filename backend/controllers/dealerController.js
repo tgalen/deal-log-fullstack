c3728a681e0e56021e977666beac05fe2fbda9d5
tgalen <tgalen84@gmail.com>
2024-11-05 20:13:31 -0500

    add express async handler to controller functions

--- backend/controllers/dealerController.js
+++ backend/controllers/dealerController.js
@@ -1,22 +1,24 @@
+const asyncHandler = require("express-async-handler");
+
 // GET
-const getDealers = (req, res) => {
+const getDealers = asyncHandler((req, res) => {
   res.status(200).json({ message: "Get dealers" });
-};
+});
 
 // POST
-const createDealer = (req, res) => {
+const createDealer = asyncHandler((req, res) => {
   res.status(200).json({ message: "Dealer created" });
-};
+});
 
 // PUT
-const updateDealer = (req, res) => {
+const updateDealer = asyncHandler((req, res) => {
   res.status(200).json({ message: `Update dealer ${req.params.id}` });
-};
+});
 
 // DELETE
-const deleteDealer = (req, res) => {
+const deleteDealer = asyncHandler((req, res) => {
   res.status(200).json({ message: `Delete dealer ${req.params.id}` });
-};
+});
 
 module.exports = {
   getDealers,
