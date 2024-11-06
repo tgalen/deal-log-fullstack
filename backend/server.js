aaddbb01a6945b315323dad4d7a59cde0694b859
tgalen <tgalen84@gmail.com>
2024-11-06 07:18:01 -0500

    add db.js, connect to mongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
 const port = process.env.PORT || 5000;
+const connectDB = require("./config/db");
+
+connectDB();
 
 const app = express();
 
