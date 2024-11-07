7b76209814e671c262ca2d7eebb3caf634f9f0a8
tgalen <tgalen84@gmail.com>
2024-11-07 13:55:51 -0500

    add urlencoded method to server.js, add functionality to createDealer controller

--- backend/controllers/dealerController.js
+++ backend/controllers/dealerController.js
@@ -3,22 +3,34 @@ const asyncHandler = require("express-async-handler");
 const Dealer = require("../models/dealerModel");
 
 // GET
-const getDealers = asyncHandler((req, res) => {
-  res.status(200).json({ message: "Get dealers" });
+const getDealers = asyncHandler(async (req, res) => {
+  const dealers = await Dealer.find();
+
+  res.status(200).json(dealers);
 });
 
 // POST
-const createDealer = asyncHandler((req, res) => {
-  res.status(200).json({ message: "Dealer created" });
+const createDealer = asyncHandler(async (req, res) => {
+  //   console.log(req.body);
+
+  if (!req.body.dealerName) {
+    res.status(400);
+    throw new Error("Please add a text field.");
+  }
+
+  const dealer = await Dealer.create({
+    dealerName: req.body.dealerName,
+  });
+  res.status(200).json({ dealer });
 });
 
 // PUT
-const updateDealer = asyncHandler((req, res) => {
+const updateDealer = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Update dealer ${req.params.id}` });
 });
 
 // DELETE
-const deleteDealer = asyncHandler((req, res) => {
+const deleteDealer = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Delete dealer ${req.params.id}` });
 });
 
--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,9 @@ connectDB();
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.use("/api/dealers", require("./routes/dealerRoutes"));
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
