3b950df6978d753449de46e0eedde64dc7dffce7
tgalen <tgalen84@gmail.com>
2024-11-07 18:30:45 -0500

    embed deal data in dealer model

--- backend/models/dealerModel.js
+++ backend/models/dealerModel.js
@@ -13,6 +13,29 @@ const dealerSchema = mongoose.Schema({
   dealerAdmin: {
     type: String,
   },
+  deals: [
+    {
+      dealNumber: { type: Number },
+      customerName: { type: String },
+      date: { type: String },
+      vin: { type: String },
+      flc: { type: String },
+      buyRate: { type: Number },
+      sellRate: { type: Number },
+      reserve: { type: Number },
+      vsc: { type: Number },
+      gap: { type: Number },
+      tireWheel: { type: Number },
+      key: { type: Number },
+      dent: { type: Number },
+      windshield: { type: Number },
+      xsWear: { type: Number },
+      advancedProtection: { type: Number },
+      advancedProtectionPlus: { type: Number },
+      multiCoverage: { type: Number },
+      totalBackendGross: { type: Number },
+    },
+  ],
 });
 
 module.exports = mongoose.model("Dealer", dealerSchema);
