87a79d52d89a59b81a13e5c2af29c746c9fde9c0
tgalen <tgalen84@gmail.com>
2024-11-11 21:33:56 -0500

    add loginUser and getMe

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,4 +2,12 @@ const registerUser = async (req, res) => {
   res.json({ message: "Register User" });
 };
 
-module.exports = { registerUser };
+const loginUser = async (req, res) => {
+  res.json({ message: "Login user" });
+};
+
+const getMe = async (req, res) => {
+  res.json({ message: "Get user data" });
+};
+
+module.exports = { registerUser, loginUser, getMe };
