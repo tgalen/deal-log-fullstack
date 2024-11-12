28eb029779d68989f1ea7678e86aa9bbd7bfc0b5
tgalen <tgalen84@gmail.com>
2024-11-11 21:17:16 -0500

    add role key to User model

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -15,6 +15,9 @@ const userSchema = mongoose.Schema(
       type: String,
       required: [true, "Please add a password"],
     },
+    role: {
+      type: String, //produer, sales manager, director, accounting, GM
+    },
   },
   {
     timestamps: true,
