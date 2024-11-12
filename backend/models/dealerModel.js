473fc096792ae32c7869154467084464700c5cb9
tgalen <tgalen84@gmail.com>
2024-11-11 21:14:56 -0500

    add ObjectId as data type in users and dealerAdmin keys

--- backend/models/dealerModel.js
+++ backend/models/dealerModel.js
@@ -1,56 +1,72 @@
 const mongoose = require("mongoose");
 
-const dealerSchema = mongoose.Schema({
-  dealerName: {
-    type: String,
-  },
-  dealerGroup: {
-    type: String,
-  },
-  users: [
-    {
-      name: { type: String },
-      isActive: { type: Boolean },
-    },
-  ],
-  dealerAdmin: [
-    {
+const dealerSchema = mongoose.Schema(
+  {
+    dealerName: {
       type: String,
-      isActive: Boolean,
     },
-  ],
-  deals: [
-    {
-      dealNumber: { type: Number },
-      customerName: { type: String },
-      dateOfDelivery: { type: String },
-      vin: { type: String },
-      flc: { type: String },
-      lender: { type: String },
-      term: { type: Number },
-      newOrUsed: { type: String },
-      buyRate: { type: Number },
-      sellRate: { type: Number },
-      reserve: { type: Number },
-      vsc: {
-        gross: { type: Number },
-        remitted: { type: Boolean },
+    dealerGroup: {
+      type: String,
+    },
+    users: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        isActive: { type: Boolean },
+        ref: "User",
       },
-      gap: { type: Number },
-      tireWheel: { type: Number },
-      key: { type: Number },
-      dent: { type: Number },
-      windshield: { type: Number },
-      xsWear: { type: Number },
-      advancedProtection: { type: Number },
-      advancedProtectionPlus: { type: Number },
-      multiCoverage: { type: Number },
-      totalBackendGross: { type: Number },
-      funded: { type: Boolean },
-      financeManager: { type: String },
-      salesperson: { type: String },
-    },
-  ],
-});
+      {
+        timestamps: true,
+      },
+    ],
+    dealerAdmin: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        isActive: { type: Boolean },
+        ref: "User",
+      },
+      {
+        timestamps: true,
+      },
+    ],
+    deals: [
+      {
+        dealNumber: { type: Number },
+        customerName: { type: String },
+        dateOfDelivery: { type: String },
+        vin: { type: String },
+        flc: { type: String },
+        lender: { type: String },
+        term: { type: Number },
+        newOrUsed: { type: String },
+        buyRate: { type: Number },
+        sellRate: { type: Number },
+        reserve: { type: Number },
+        vsc: {
+          gross: { type: Number },
+          remitted: { type: Boolean },
+        },
+        gap: { type: Number },
+        tireWheel: { type: Number },
+        key: { type: Number },
+        dent: { type: Number },
+        windshield: { type: Number },
+        xsWear: { type: Number },
+        advancedProtection: { type: Number },
+        advancedProtectionPlus: { type: Number },
+        multiCoverage: { type: Number },
+        totalBackendGross: { type: Number },
+        funded: { type: Boolean },
+        financeManager: { type: String },
+        salesperson: { type: String },
+      },
+      {
+        timestamps: true,
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("Dealer", dealerSchema);
