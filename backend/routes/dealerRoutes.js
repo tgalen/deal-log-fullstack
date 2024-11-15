7a55cb3483a3a040367e760bc010447595975fb2
tgalen <tgalen84@gmail.com>
2024-11-14 21:50:43 -0500

    protect createDealers route

--- backend/routes/dealerRoutes.js
+++ backend/routes/dealerRoutes.js
@@ -8,7 +8,7 @@ const {
 } = require("../controllers/dealerController");
 const { protect } = require("../middleware/authMiddleware");
 
-router.route("/").get(protect, getDealers).post(createDealer);
+router.route("/").get(protect, getDealers).post(protect, createDealer);
 router.route("/:id").put(protect, updateDealer).delete(protect, deleteDealer);
 
 module.exports = router;
