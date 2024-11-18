46fbe572ad90f93362e5faa6d295a1d0fdee11ee
tgalen <tgalen84@gmail.com>
2024-11-18 17:52:19 -0500

    useState lockedIn user

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Landing from "./components/Landing";
@@ -5,6 +6,9 @@ import Register from "./components/Register";
 import Login from "./components/Login";
 
 function App() {
+  const [lockedInLoggedInUser, setLockedInLoggedInUser] = useState(null);
+  const lockedInUser = JSON.parse(localStorage.getItem("lockedInUser"));
+
   return (
     <div className="App">
       <BrowserRouter>
