9efe4621b471ec644a1497c8666980f32cb3d87a
tgalen <tgalen84@gmail.com>
2024-11-25 18:17:26 -0500

    getUserDealers on load via useEffect

--- frontend/src/components/Homepage.js
+++ frontend/src/components/Homepage.js
@@ -1,10 +1,33 @@
+import { useState, useEffect } from "react";
 import Container from "@mui/material/Container";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
+import axios from "axios";
+import { DEALERS_API } from "../constants/constants";
 
 const HomePage = ({ loggedInLockedInUser }) => {
+  const [userDealers, setUserDealers] = useState(null);
+  const config = loggedInLockedInUser && {
+    headers: {
+      Authorization: `Bearer ${loggedInLockedInUser.token}`,
+    },
+  };
+
+  const getUserDealers = async () => {
+    const response = await axios.get(DEALERS_API, config);
+
+    if (response) {
+      setUserDealers(response.data);
+    }
+  };
+
+  useEffect(() => {
+    loggedInLockedInUser && getUserDealers();
+  }, []);
+  console.log(userDealers);
+
   return (
     <Container maxWidth="sm">
       <Card sx={{ display: "flex", marginTop: "20px" }}>
--- frontend/src/constants/constants.js
+++ frontend/src/constants/constants.js
@@ -1,3 +1,5 @@
 export const REGISTER_API = "http://localhost:5000/api/users";
 
 export const LOGIN_API = "http://localhost:5000/api/users/login";
+
+export const DEALERS_API = "http://localhost:5000/api/dealers";
